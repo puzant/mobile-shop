d9f7e8f8366261678612581d3033bfe4c7763317
Steve <sql.leb@gmail.com>
2017-07-08 15:19:49 +0300

    login auth work update

--- login/loginApp.js
+++ login/loginApp.js
@@ -1,5 +1,14 @@
-var app = angular.module("app", []);
+var app = angular.module("app");
 
 app.controller('loginCtrl', function($scope) {
+
+$scope.login = function (user) {
+  $http.post("http://localhost/auth/login").then(function(res){
+      $location.path("/test")
+    },
+  function(response) {
+    alert('login succeed' + response.data.message);
+});
+}
 
 })
