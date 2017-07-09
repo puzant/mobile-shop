06723373876c969230a09b44bc34f5321ebb2010
puzant <PBAKJEJIAN@hotmail.com>
2017-07-09 22:30:31 +0300

    setting the front end auth

--- signup/signup.js
+++ signup/signup.js
@@ -1,13 +1,32 @@
-var app = angular.module("app");
+//var app = angular.module("app.Auth");
+//
+//app.controller('signupCtrl', ["$scope", "$location", "UserService"] function($scope, $http, $location) {
+//    
+//    $scope.submit = function(user) {
+//        $http.post('http://localhost:8000/auth/signup', user).then(function(res) {
+//            $location.path('/login')
+//        }, function(response) {
+//            alert('there was problem in the operation' + response.data.message);
+//        }); 
+//    }
+//    
+//})
 
-app.controller('signupCtrl', function($scope, $http, $location) {
+
+var app = angular.module("app.Auth");
+
+app.controller("signupCtrl", ["$scope", "$location", "UserService", function($scope, $location, UserService) {
+    $scope.passwordMessage = "";
     
     $scope.submit = function(user) {
-        $http.post('http://localhost:8000/auth/signup', user).then(function(res) {
-            $location.path('/login')
-        }, function(response) {
-            alert('there was problem in the operation' + response.data.message);
-        }); 
+        if(user.password !== $scope.passwordRepeat) {
+            $scope.passwordMessage = "password does not match";
+        } else {
+            UserService.signup(user).then(function(res) {
+                $location.path('/login');
+            }, function(res) {
+                alert('there was a problem' + res.data);
+            });
+        }
     }
-    
-})
+}]);
