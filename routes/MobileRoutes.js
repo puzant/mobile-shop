711d0eb3e9f5f5feab3e856c7c04bed80c33fa11
puzant <PBAKJEJIAN@hotmail.com>
2017-07-11 20:01:29 +0300

    setting the backend

--- routes/MobileRoutes.js
+++ routes/MobileRoutes.js
@@ -1,8 +1,17 @@
-//var express = require('express');
-//var MobileRoutes = express.Router();
-//
-//
-//
-//MobileRoutes.post('/', function(req, res) {
-//    
-//})
+var express = require('express');
+var mobileRoutes = express.Router();
+var mobile = require('./models/mobile')
+
+mobileRoutes.post('/', function(req, res) {
+  var newPhone = new mobile(req.body);
+    newPhone.save(function(err, phone) {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.send(phone);
+        }
+    })
+});
+
+
+module.exports = mobileRoutes;
