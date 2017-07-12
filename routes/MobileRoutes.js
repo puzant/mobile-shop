b7105376cd5e1caa8368679ec1b3f7d0f694beb3
puzant <PBAKJEJIAN@hotmail.com>
2017-07-12 16:05:14 +0300

    making changes

--- routes/MobileRoutes.js
+++ routes/MobileRoutes.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mobileRoutes = express.Router();
 var mobile = require('../models/mobile');
 
+
 mobileRoutes.post('/', function(req, res) {
   var newPhone = new mobile(req.body);
     newPhone.save(function(err, phone) {
@@ -12,6 +13,18 @@ mobileRoutes.post('/', function(req, res) {
         }
     })
 });
+
+
+mobileRoutes.route('/')
+.get(function(req, res){
+    mobile.find(function(err, workout){
+        if(err){
+            res.status(500).send(err);
+        } else {
+            res.send(workout)
+        }
+    })
+})
 
 
 module.exports = mobileRoutes;
