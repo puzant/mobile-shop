998b02bda7d74134eddde40d90cc101b6e46656f
puzant <PBAKJEJIAN@hotmail.com>
2017-07-12 16:58:59 +0300

    modified angular file

--- viewes/store/store.js
+++ viewes/store/store.js
@@ -5,3 +5,14 @@
 //        console.log(res.data);
 //    })
 //})
+
+var app = angular.module("app");
+
+app.controller("storeCtrl", function ($scope, $http) {
+    // get data from the database
+    $http.get('http://localhost:8000/api/mobile').then(function (res) {
+        console.log(res.data); //the api object
+        //$scope.workouts = res.data;
+    })
+    
+})
